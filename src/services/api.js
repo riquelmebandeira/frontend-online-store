0d24d46e6809d776ea21db5cba528033c0c8ebb1
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-09-17 17:01:09 -0300

    req 1 e 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
+// Chama API das categorias { Todos do grupo presentes }
 export async function getCategories() {
-  // Implemente aqui
+  const categUrl = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const fetchCateg = await fetch(categUrl);
+  const respCateg = await fetchCateg.json();
+
+  return respCateg;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui!
+// Chama API dos produtos por categoria/termo { Todos do grupo presentes }
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
+  const productUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  const fetchProduct = await fetch(productUrl);
+  const respProduct = await fetchProduct.json();
+
+  return respProduct;
 }
